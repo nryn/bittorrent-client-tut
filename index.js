27d789ee7e8fc012041e3bf3edab1b497b3dfb22
nryn <narayanohanlon@gmail.com>
2017-04-24 22:02:46 +0100

    add message.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
+
+const download = require('./src/download');
 const fs = require('fs');
 const bencode = require('bencode');
-const tracker = require('./tracker');
-const torrentParser = require('./torrent-parser');
+const tracker = require('./src/tracker');
+const torrentParser = require('./src/torrent-parser');
 
-const torrent = torrentParser.open('flagfromserver.torrent');
+const torrent = torrentParser.open(process.argv[2]);
 
-tracker.getPeers(torrent, peers => {
-  console.log('list of peers: ', peers);
-});
+download(torrent);
