71f5b3adce429751df6c7bd3c4f0cfb2f52c4c33
nryn <narayanohanlon@gmail.com>
2017-04-24 23:31:26 +0100

    add pieces

--- src/download.js
+++ src/download.js
@@ -6,18 +6,20 @@ const tracker = require('./tracker');
 const message = require('./message');
 
 module.exports = torrent => {
+  const requested = [];
   tracker.getPeers(torrent, peers => {
-    peers.forEach(peer => download(peer, torrent));
+    peers.forEach(peer => download(peer, torrent, requested));
   });
 };
 
-function download(peer, torrent) {
+function download(peer, torrent, requested) {
   const socket = new net.Socket();
   socket.on('error', console.log);
   socket.connect(peer.port, peer.ip, () => {
     socket.write(message.buildHandshake(torrent));
   });
-  onWholeMsg(socket, msg => msgHandler(msg, socket));
+  const queue = [];
+  onWholeMsg(socket, msg => msgHandler(msg, socket, requested, queue));
 }
 
 function onWholeMsg(socket, callback) {
@@ -37,11 +39,58 @@ function onWholeMsg(socket, callback) {
   });
 }
 
-function msgHandler(msg, socket) {
-  if (isHandshake(msg)) socket.write(message.buildInterested());
-}
-
 function isHandshake(msg) {
   return msg.length === msg.readUInt8(0) + 49 &&
          msg.toString('utf8', 1) === 'BitTorrent protocol';
+}
+
+function msgHandler(msg, socket, requested, queue) {
+  if (isHandshake(msg)) {
+    socket.write(message.buildInterested());
+  } else {
+    const m = message.parse(msg);
+
+    if (m.id === 0) chokeHandler();
+    if (m.id === 1) unchokeHandler();
+    if (m.id === 4) haveHandler(m.payload, socket, requested, queue);
+    if (m.id === 5) bitfieldHandler(m.payload);
+    if (m.id === 7) pieceHandler(m.payload, socket, requested, queue);
+  }
+}
+
+
+// function chokeHandler() { ... }
+//
+// function unchokeHandler() { ... }
+
+function haveHandler(payload, socket, requested, queue) {
+  // ...
+  const pieceIndex = payload.readUInt32BE(0);
+  queue.push(pieceIndex);
+  if (queue.length === 1) {
+    requestPiece(socket, requested, queue);
+  }
+  // previous code, not sure if should be deleted
+  // if (!requested[pieceIndex]) {
+  //   socket.write(message.buildRequest(...));
+  // }
+  // requested[pieceIndex] = true;
+}
+
+// function bitfieldHandler(payload) { ... }
+
+function pieceHandler(payload, socket, requested, queue) {
+  // ...
+  queue.shift();
+  requestPiece(socket, requested, queue);
+}
+
+function requestPiece(socket, requested, queue) {
+  if (requested[queue[0]]) {
+    queue.shift();
+  } else {
+    // this is pseudo-code, as buildRequest actually takes slightly more
+    // complex arguments
+    socket.write(message.buildRequest(pieceIndex));
+  }
 }
